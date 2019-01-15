8b9aac22b5088e6bc609f429c767025a31c3eb0b
Rodimiro Cerrato E <rodyce@gmail.com>
2019-01-15 03:09:30 -0600

    Add wallet generation logic
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- wallet-gen.js
+++ wallet-gen.js
@@ -2,35 +2,47 @@ const bip39 = require("bip39");
 const bip32 = require("ripple-bip32");
 const ripple = require('ripplelib')
 const sign = require('ripple-sign-keypairs')
+const readline = require("readline")
 
-var mnemonic = 'novel matter final only nice cheese address cradle civil crash great flame struggle consider crowd surface purpose saddle mango endless mixed trial tape wrap'
-// Or generate:
-// mnemonic = bip39.generateMnemonic()
+// Sample mnemonics to Use (input at prompt):
+// kit youth enroll gravity inform coil life response over collect shrimp fashion desk million differ style october hill first fiscal reform among fiscal word
+// First generated address should be: rHZwwXzBJSw58vczQVsp4LospbspYjS5f
 
-console.log('mnemonic: ' + mnemonic)
-const seed = bip39.mnemonicToSeed(mnemonic)
-const m = bip32.fromSeedBuffer(seed)
-const keyPair = m.derivePath("m/44'/144'/0'/0/0").keyPair.getKeyPairs()
-const key = ripple.KeyPair.from_json(keyPair.privateKey.substring(2)) 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true
+})
 
-console.log('privateKey: ' + keyPair.privateKey)
-console.log('privateKeyWif: ' + key.to_pri_string()) // to_wif
-console.log('publicKey: ' + keyPair.publicKey)
-console.log('address: ' + key.to_address_string())
+const CoinCode = 144;
 
-// Now sign a transaction...
-var tx = {
-    TransactionType: 'Payment',
-    Account: key.to_address_string(),
-    Fee : (0.000012 * 1000 * 1000) + '',
-    Destination: 'rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDY',
-    DestinationTag : 2,
-    Amount: (1 * 1000 * 1000) + '',
-    
-    Sequence: 0
- }
-var txJSON = JSON.stringify(tx)
-var txSign = sign(txJSON, keyPair)
+console.log("Enter mnemonics (empty for random): ")
+rl.on('line', function(words) {
+    var mnemonic = undefined;
+    if (words.trim().length == 0) {
+        // Create random words.
+        // Use strength of 256 bits = 24 words.
+        mnemonic = bip39.generateMnemonic(256);
+    } else {
+        // Verify that the mnemonics are valid.
+        if (bip39.validateMnemonic(words)) {
+            mnemonic = words;
+        } else {
+            throw Error("Invalid mnemonics");
+        }
+    }
 
-console.log(tx)
-console.log(txSign)
+    const seed = bip39.mnemonicToSeed(mnemonic);
+    const m = bip32.fromSeedBuffer(seed);
+    for (var i = 0; i < 10; i++) {
+        var path = `m/44'/${CoinCode}'/0'/0/${i}`;
+        const keyPair = m.derivePath(path).keyPair.getKeyPairs();
+        const key = ripple.KeyPair.from_json(keyPair.privateKey.substring(2));
+
+        console.log('address:              ' + key.to_address_string());
+        console.log('(pub, priv) key pair: ' + keyPair.publicKey + ', ' + keyPair.privateKey);
+        console.log('priv key WIF:         ' + key.to_wif());
+    }
+
+    console.log("\nEnter mnemonics (empty for random): ");
+});
